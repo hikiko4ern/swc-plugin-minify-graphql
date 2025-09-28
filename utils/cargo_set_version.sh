8bb43d94c2a9f74e0ad125ba669c2604510cbc44
hikiko4ern <hk@doggo.moe>
2025-09-28 21:58:57 +0700

    chore(dev): use `cargo-set-version` instead of `sed`

--- utils/cargo_set_version.sh
+++ utils/cargo_set_version.sh
@@ -7,5 +7,4 @@ if [ -z "$npm_package_version" ]; then
 	exit 1
 fi
 
-sed -i -e "0,/version/{s/\(version\s*=\s*\"\)[^\"]*\(\"\)/\1${npm_package_version}\2/}" Cargo.toml
-cargo tree -i swc-plugin-minify-graphql # this will update crate's version in Cargo.lock
+cargo bin cargo-set-version --exclude graphql-minify "$npm_package_version"
